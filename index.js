44d2d9405a9c0edef6712569dae0387405cf054a
Yeeziie <99999949+Yeeziie@users.noreply.github.com>
2022-02-19 14:08:07 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ client.on('ready', () => {
     }, 120_000);
 
     sync();
-    setInterval(sync, 15000);
+    setInterval(sync, 5000);
 
     const { version } = require('./package.json');
     client.user.setActivity(`Vinted BOT | v${version}`);
